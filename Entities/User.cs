d0e3571b4398bbf1f7ff7368874106418e777676
Dushyantha Kalehewatta <Ulindu.Dushyantha@softlogiclife.lk>
2021-12-11 11:36:34 +0530

    Add project files.

---
+++ Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace WebApi.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+
+        [JsonIgnore]
+        public string Password { get; set; }
+
+        [JsonIgnore]
+        public List<RefreshToken> RefreshTokens { get; set; }
+    }
+}
